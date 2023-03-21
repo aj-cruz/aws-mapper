50fd2b57db7d9f25b42cb1b8d65ad6621e20ad7e
AJ Cruz <aj-cruz@users.noreply.github.com>
2023-03-20 20:06:34 -0500

    update

--- map.py
+++ map.py
@@ -448,10 +448,9 @@ def add_prefix_lists_to_word_doc():
                     child_model['table']['rows'].append({"cells":this_rows_cells})
                     # Add the child table to the parent table
                     parent_model['table']['rows'].append({"cells":[child_model]})
-                # Model has been build, now convert it to a python-docx Word table object
-                table = build_table(doc_obj, parent_model)
-                jprint(data=parent_model)
-                replace_placeholder_with_table(doc_obj, "{{py_prefix_lists}}", table)
+            # Model has been build, now convert it to a python-docx Word table object
+            table = build_table(doc_obj, parent_model)
+            replace_placeholder_with_table(doc_obj, "{{py_prefix_lists}}", table)
 
 def add_subnets_to_word_doc():
     # Create the parent table model
